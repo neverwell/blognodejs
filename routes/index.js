e35a92e09d62c843b5f3a7fdfbea7ecd7611f2d3
neverwell <292519287@qq.com>
2016-08-23 11:07:57 +0800

    实现留言功能

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ var crypto = require('crypto');
 //var User = require('../models/user.js');
 var User = require('../models/user');
 var Post = require('../models/post');
-
+var Comment = require('../models/comment.js');
 
 
 //原版本使用方法
@@ -302,6 +302,30 @@ module.exports = function(app) {
             res.redirect('/');
         });
     });
+
+    app.post('/u/:name/:day/:title', function(req, res) {
+        var date = new Date(),
+            time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+            date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+        var comment = {
+            name: req.body.name,
+            email: req.body.email,
+            website: req.body.website,
+            time: time,
+            content: req.body.content
+        };
+        var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+        newComment.save(function(err) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            req.flash('success', '留言成功!');
+            res.redirect('back');
+        });
+    });
+
+
 
 
     function checkLogin(req, res, next) {
