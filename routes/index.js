fc81f7221ada9fcd3195a6004de5f7bbcc2b4ac0
neverwell <292519287@qq.com>
2016-08-24 13:05:42 +0800

    增加404页面

--- routes/index.js
+++ routes/index.js
@@ -419,6 +419,11 @@ module.exports = function(app) {
     });
 
 
+
+    app.use(function(req, res) {
+        res.render("404");
+    });
+
     function checkLogin(req, res, next) {
         if (!req.session.user) {
             req.flash('error', '未登录!');
