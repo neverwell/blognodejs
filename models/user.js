079d6a6c07d71a6bab592e892fe7ffb8312aab7d
neverwell <292519287@qq.com>
2016-08-29 14:24:10 +0800

    使用 Async

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongodb = require('./db');
 var crypto = require('crypto');
+var async = require('async');
 /*
 User：User 是一个描述数据的对象，即 MVC 架构中的模型。
 前面我们使用了许多视图和控制器，这是第一次接触到模型。
@@ -30,54 +31,57 @@ User.prototype.save = function(callback) {
         email: this.email,
         head: head
     };
-    //打开数据库
-    mongodb.open(function(err, db) {
-        if (err) {
-            return callback(err); //错误，返回 err 信息
-        }
-        //读取 users 集合
-        db.collection('users', function(err, collection) {
-            if (err) {
-                mongodb.close();
-                return callback(err); //错误，返回 err 信息
-            }
+    async.waterfall([
+        function(cb) {
+            //打开数据库
+            mongodb.open(function(err, db) {
+                cb(err, db);
+            });
+        },
+        function(db, cb) {
+            //读取 users 集合
+            db.collection('users', function(err, collection) {
+                cb(err, collection);
+            });
+        },
+        function(collection, cb) {
             //将用户数据插入 users 集合
             collection.insert(user, {
                 safe: true
             }, function(err, user) {
-                mongodb.close();
-                if (err) {
-                    return callback(err); //错误，返回 err 信息
-                }
-                callback(null, user[0]); //成功！err 为 null，并返回存储后的用户文档
+                cb(err, user);
             });
-        });
+        }
+    ], function(err, user) {
+        mongodb.close();
+        callback(err, user[0]);
     });
 };
-
 //读取用户信息
 User.get = function(name, callback) {
-    //打开数据库
-    mongodb.open(function(err, db) {
-        if (err) {
-            return callback(err); //错误，返回 err 信息
-        }
-        //读取 users 集合
-        db.collection('users', function(err, collection) {
-            if (err) {
-                mongodb.close();
-                return callback(err); //错误，返回 err 信息
-            }
+    async.waterfall([
+        function(cb) {
+            //打开数据库
+            mongodb.open(function(err, db) {
+                cb(err, db);
+            });
+        },
+        function(db, cb) {
+            //读取 users 集合
+            db.collection('users', function(err, collection) {
+                cb(err, collection);
+            });
+        },
+        function(collection, cb) {
             //查找用户名（name键）值为 name 一个文档
             collection.findOne({
                 name: name
             }, function(err, user) {
-                mongodb.close();
-                if (err) {
-                    return callback(err); //失败！返回 err 信息
-                }
-                callback(null, user); //成功！返回查询的用户信息
+                cb(err, user);
             });
-        });
+        }
+    ], function(err, user) {
+        mongodb.close();
+        callback(err, user);
     });
 };
